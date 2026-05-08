fe03c4ed17b1af6adcc18bc5dc660bf3b55e3e7c
Venukumar23 <venurakati@gmail.com>
2026-05-08 21:24:47 +0530

    Add folder size utility

---
+++ backend/utils/folderSize.js
@@ -0,0 +1,18 @@
+const Folder = require('../models/Folder');
+const Image = require('../models/Image');
+
+// Recursively calculate the total size of a folder and its subfolders
+async function getFolderSize(folderId, userId) {
+  let totalSize = 0;
+  // Get all images in this folder
+  const images = await Image.find({ folderId, userId });
+  totalSize += images.reduce((sum, img) => sum + img.size, 0);
+  // Get all subfolders
+  const subfolders = await Folder.find({ parentFolderId: folderId, userId });
+  for (const subfolder of subfolders) {
+    totalSize += await getFolderSize(subfolder._id, userId);
+  }
+  return totalSize;
+}
+
+module.exports = getFolderSize;
